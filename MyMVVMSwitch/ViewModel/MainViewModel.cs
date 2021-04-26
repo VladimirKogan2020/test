463e9c5e5a54542c0410da6990cfb6bbb39d587e
VladimirKogan2020 <37444210+VladimirKogan2020@users.noreply.github.com>
2021-04-26 23:25:59 +0200

    Add files via upload

---
+++ MyMVVMSwitch/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MyMVVMSwitch.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyMVVMSwitch.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel selectedViewModel;
+
+        public BaseViewModel SelectedViewModel
+        {
+            get { return selectedViewModel; }
+            set
+            {
+                selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+
+        public ICommand UpdateViewCommand { get; set; }
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);  
+        }
+
+    }
+}
